1e632759452938d97268a4889c24cb72a8be2f59
Dikshyanta Uprety <dikshyantauprety@Dikshyantas-MacBook-Pro.local>
2023-08-13 06:16:37 +0100

    first commit

---
+++ src/books.js
@@ -0,0 +1,26 @@
+import img1 from './images/book1.jpeg';
+import img2 from './images/book2.jpeg';
+import img3 from './images/book3.jpeg';
+
+const books = [
+  {
+    id: 1,
+    author: 'Gabrielle Zevin',
+    title: 'Tomorrow, and Tomorrow, and Tomorrow',
+    image:img1 ,
+  },
+  {
+    id: 2,
+    author: 'Cara Hunter',
+    title: 'Murder in the Family',
+    image:img2,
+  },
+  {
+    id: 3,
+    author: 'Colleen Hoover',
+    title: 'It ends with us',
+    image:img3,
+  },
+];
+
+export default books;
